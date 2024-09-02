2175b056e22fa9247b09e9bcadcf3a70be880645
10206242 <wufengting@hengli.com>
2024-09-02 15:21:28 +0800

    commit

--- hk-module-system/hk-module-system-biz/src/main/java/com/hk/jigai/module/system/dal/mysql/userreport/UserReportMapper.java
+++ hk-module-system/hk-module-system-biz/src/main/java/com/hk/jigai/module/system/dal/mysql/userreport/UserReportMapper.java
@@ -1,7 +1,9 @@
 package com.hk.jigai.module.system.dal.mysql.userreport;
 
 import java.util.*;
+import java.util.function.Consumer;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.hk.jigai.framework.common.pojo.PageResult;
 import com.hk.jigai.framework.mybatis.core.query.LambdaQueryWrapperX;
 import com.hk.jigai.framework.mybatis.core.mapper.BaseMapperX;
@@ -14,6 +16,8 @@ import com.hk.jigai.module.system.dal.dataobject.userreport.UserReportDO;
 import com.hk.jigai.module.system.dal.dataobject.userreport.UserSummaryReportDO;
 import org.apache.ibatis.annotations.Mapper;
 
+import static com.hk.jigai.framework.security.core.util.SecurityFrameworkUtils.getLoginUserId;
+
 /**
  * 用户汇报 Mapper
  *
@@ -21,11 +25,15 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface UserReportMapper extends BaseMapperX<UserReportDO> {
-
     default PageResult<UserReportDO> selectPage(UserReportPageReqVO reqVO) {
+        String userId = getLoginUserId().toString();
         return selectPage(reqVO, new LambdaQueryWrapperX<UserReportDO>()
                 .betweenIfPresent(UserReportDO::getDateReport, reqVO.getDateReport())
                 .likeIfPresent(UserReportDO::getUserNickName, reqVO.getUserNickName())
+                .and(ss -> ss.like(UserReportDO::getReportObject, userId)
+                        .or().likeLeft(UserReportDO::getReportObject, userId + "]")
+                        .or().likeRight(UserReportDO::getReportObject, "[" + userId)
+                        .or().eq(UserReportDO::getReportObject, "[" + userId + "]"))
                 .orderByDesc(UserReportDO::getId));
     }
 
